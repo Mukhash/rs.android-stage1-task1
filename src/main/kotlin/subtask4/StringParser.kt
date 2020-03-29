dd4b5d1d25cab50318c81765f5d44ecf595bca90
Latala <muhash.astana@mail.ru>
2020-03-29 20:49:55 +0600

    Task 4 tests 1, 2 done

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -1,9 +1,35 @@
 package subtask4
 
+import java.lang.Integer.min
+import java.util.*
+
 class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val array:MutableList<String> = mutableListOf()
+        val tempArray = inputString.toList().map {it.toString()}
+        val mapOfOpen:SortedMap<Int, String> = sortedMapOf()
+        val mapOfClose:MutableMap<Int, String> = mutableMapOf()
+        val mapOfSigns: Map<String, String> = mapOf("<" to ">", "[" to "]", "(" to ")")
+        var open = 0
+        var close = 0
+
+        for (i in tempArray.indices) {
+            if (tempArray[i] == "<" || tempArray[i] == "(" || tempArray[i] == "[") {
+                mapOfOpen[i] = tempArray[i]
+            } else if (tempArray[i] == ">" || tempArray[i] == ")" || tempArray[i] == "]") {
+                mapOfClose[i] = tempArray[i]
+            }
+        }
+
+        for ((k, v) in mapOfOpen) {
+            open = k
+            close = mapOfClose.filterValues { it == mapOfSigns[v] }.keys.min()!!
+            array.add(inputString.substring(open + 1, close))
+            mapOfClose.remove(close)
+        }
+
+        return array.toTypedArray()
     }
 }
